834bc1744a7fd392af84f4e46f8db31322af8d42
SaiaKhan <crimsun2009@yahoo.de>
2018-02-28 15:44:23 +0100

    spacing

--- log_uploader.py
+++ log_uploader.py
@@ -5,6 +5,7 @@ import json
 import glob
 import pyperclip
 
+
 class log_uploader(object):
     def __init__(self):
         self.log_folder = os.path.join(os.path.expanduser("~"), "Documents",
@@ -36,6 +37,7 @@ class log_uploader(object):
         self.response = result
         self.parse_response(result)
 
+
     def get_latest_file(self, dir):
         #print("searching directory: " + glob.glob(dir+"*")[0])
         max_ctime = 0
@@ -49,11 +51,13 @@ class log_uploader(object):
                     max_file = fname
         return os.path.join(max_file)#max_dir, max_file)
 
+
     def make_dirlist(self, namelist):
         result = []
         for name in namelist:
             result.append(glob.glob(os.path.join(self.log_folder, name+"*"))[0])
         return result
+
 
     def make_filelist(self, dirlist):
         result = []
@@ -61,19 +65,23 @@ class log_uploader(object):
             result.append(os.path.join(dir, self.get_latest_file(dir)))
         return result
 
+
     def upload_fractals(self):
         files = self.make_filelist(self.make_dirlist(self.fractals))
         self.upload_logs(files)
 
+
     def upload_raids(self):
         files = self.make_filelist(self.make_dirlist(self.raids))
         self.upload_logs(files)
+
 
     def parse_response(self, responses):
         result = """** %s ** \n""" % (datetime.datetime.now().strftime('%d/%m/%y %H:%M'))
         for r in responses:
             result = result + self.bosses[str(r["metadata"]["evtc"]["bossId"])] + ": " + r["permalink"] + "\n"
         pyperclip.copy(result)
+
 
     def upload_test(self):
         files = self.make_filelist(self.make_dirlist(["Xera"]))
