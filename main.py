be72b6fac499b5f30a3e715f776c3111cc12814a
SaiaKhan <crimsun2009@yahoo.de>
2018-03-06 04:24:49 +0100

    ui stuff
    Groundwork for generating bosslist widget from a file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("---------- Starting GW2 Log Uploader ----------")
 
 import sys
-from PyQt5 import QtWidgets, Qt, QtGui
+from PyQt5 import QtWidgets, QtCore, QtGui, Qt
 from mainwindow import Ui_MainWindow as MainWindow
 #from log_uploader import log_uploader
 import configparser
@@ -14,12 +14,31 @@ class MyMainWindow(QtWidgets.QMainWindow, MainWindow):
         print("Loaded UI!")
 
         config = configparser.ConfigParser()
-        config.read("options.ini")
+        self.options = config.read("options.ini")
 
-        for widget in self.frame_bosses.children():
-            if isinstance(widget, QtWidgets.QGroupBox):
-                print(widget.objectName())
-                #widget.setEnabled(True)
+        self.populate_treeview()
+
+        # This makes the checkbox border light-blue
+        treePalette = QtGui.QPalette()
+        treePalette.setColor(QtGui.QPalette.Window, QtGui.QColor(85, 170, 255))
+        self.treeWidget.setPalette(treePalette)
+
+        #for widget in self.frame_bosses.children():
+        #    if isinstance(widget, QtWidgets.QGroupBox):
+        #        print(widget.objectName())
+
+    def populate_treeview(self):
+        # load boss- and wingnames from the options file
+        data = {"Spirit Vale": ["VG", "Gorseval", "Sabetha"], "Hall of Chains": ["Soulless Horror", "Dhuum"]}
+        for wing in data.keys():
+            parent = QtWidgets.QTreeWidgetItem(self.treeWidget)
+            parent.setText(0, wing)
+            parent.setFlags(QtCore.Qt.ItemIsAutoTristate | QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
+            for boss in data[wing]:
+                child = QtWidgets.QTreeWidgetItem(parent)
+                child.setFlags(QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsUserCheckable)
+                child.setText(0, boss)
+                child.setCheckState(0, QtCore.Qt.Checked)
 
 
 app = QtWidgets.QApplication(sys.argv)
@@ -39,6 +58,10 @@ dark_palette.setColor(QtGui.QPalette.BrightText, Qt.QColor(255, 0, 0))
 dark_palette.setColor(QtGui.QPalette.Link, QtGui.QColor(42, 130, 218))
 dark_palette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(42, 130, 218))
 dark_palette.setColor(QtGui.QPalette.HighlightedText, Qt.QColor(0, 0, 0))
+
+#dark_palette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(142,45,197).lighter())
+dark_palette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(85, 170, 255).lighter())
+dark_palette.setColor(QtGui.QPalette.HighlightedText, QtCore.Qt.black)
 
 app.setPalette(dark_palette)
 
