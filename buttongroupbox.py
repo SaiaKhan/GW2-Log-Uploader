5fb9bde6099f2c17381561cec5cbc6f4eaa644ae
SaiaKhan <crimsun2009@yahoo.de>
2018-03-08 07:32:16 +0100

    housekeeping

--- buttongroupbox.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-
-class ButtonGroupBox(QtWidgets.QGroupBox):
-    def __init__(self, parent=None):
-        super(ButtonGroupBox, self).__init__(parent=parent)
-        #self.groupBox = QtWidgets.QGroupBox(self)
-        self.button = QtWidgets.QPushButton("FOO", parent=self)
-        #print(self.groupBox.width())
-        self.button.move(150, -4)
